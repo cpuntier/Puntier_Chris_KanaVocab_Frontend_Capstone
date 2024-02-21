a4bfb5b208e785101f7afb2bd9d11dd8c2d53b5b
cpuntier <cpuntier@gmail.com>
2024-02-21 08:57:30 -0500

    small update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import "../styles/footer.css"
 import { useLocation } from "react-router-dom"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+
 export default function Footer() {
     const path = useLocation()
     console.log("Footer is htere0", path)
@@ -7,7 +11,9 @@ export default function Footer() {
         return (
             <>
                 <div className="footer">
-                    <p>About - Linkedin - Github - Email</p>
+                    <p>
+                     <a href="https://www.linkedin.com/in/chrispuntier/"><FaLinkedin /> Linkedin </a>   -
+                     <a href="https://github.com/cpuntier"> <FaGithub /> Github</a> </p>
                 </div>
             </>
         )
